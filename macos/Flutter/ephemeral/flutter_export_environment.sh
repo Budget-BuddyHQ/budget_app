75c529104300f3f37d69d4e0b8fa6daa89ff420b
Mid <noobability21@gmail.com>
2026-04-10 21:12:55 -0400

    flutter clean

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\bruck\develop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\bruck\OneDrive\Documents\GitHub\budget_app"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\wei34\Java without ONe\budget_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
